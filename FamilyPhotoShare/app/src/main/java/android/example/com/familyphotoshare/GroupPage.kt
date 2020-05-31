c4909d62791430f1b95a058159cfeee6603266a6
Tiffany Cole <Drakenshiinx@hotmail.com>
2020-05-31 13:27:16 -0700

    added group page

---
+++ FamilyPhotoShare/app/src/main/java/android/example/com/familyphotoshare/GroupPage.kt
@@ -0,0 +1,12 @@
+package android.example.com.familyphotoshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_page)
+    }
+}
