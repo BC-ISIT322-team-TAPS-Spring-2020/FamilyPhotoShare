97dcaf2e7b3f1e8adeaf117397a760b0f95c1aef
Sean Bruce <seanbruce@hotmail.com>
2020-06-08 13:52:15 -0700

    Select Photo merged in, Camera is having permission issues

--- FamilyPhotoShare/app/src/main/java/android/example/com/familyphotoshare/HomePage.kt
+++ FamilyPhotoShare/app/src/main/java/android/example/com/familyphotoshare/HomePage.kt
@@ -1,17 +1,23 @@
 package android.example.com.familyphotoshare
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_home_page.*
 import java.util.jar.Manifest
 
-
 class HomePage : AppCompatActivity() {
+
+    private val PERMISSION_CODE = 1000;
+    private val IMAGE_CAPTURE_CODE = 1001;
+    var image_uri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +43,35 @@ class HomePage : AppCompatActivity() {
                 pickImageFromGallery();
             }
         }
+
+        takePicture.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(android.Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED) {
+                    //permission was not enabled
+                    val permission = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE)
+                }
+                else {
+                    openCamera()
+                }
+            }
+            else {
+                openCamera()
+            }
+        }
+    }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
     }
 
     private fun pickImageFromGallery() {
@@ -54,19 +89,16 @@ class HomePage : AppCompatActivity() {
 
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             PERMISSION_CODE -> {
                 if (grantResults.size > 0 && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED
-                ) {
+                    PackageManager.PERMISSION_GRANTED) {
+                    //openCamera()
                     pickImageFromGallery()
-                } else {
+                }
+                else {
                     Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -79,4 +111,5 @@ class HomePage : AppCompatActivity() {
             imageView.setImageURI(data?.data)
         }
     }
+
 }
