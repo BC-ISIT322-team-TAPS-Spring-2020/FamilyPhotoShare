5b2e3dfbb87d334127a9904e263fd6e31ca469fe
Sean Bruce <seanbruce@hotmail.com>
2020-06-16 16:50:18 -0700

    6/16 in class work

--- FamilyPhotoShare/app/src/main/java/android/example/com/familyphotoshare/HomePage.kt
+++ FamilyPhotoShare/app/src/main/java/android/example/com/familyphotoshare/HomePage.kt
@@ -1,5 +1,6 @@
 package android.example.com.familyphotoshare
 
+import android.Manifest
 import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
@@ -12,6 +13,24 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_home_page.*
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.util.Log
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+//import com.google.firebase.referencecode.storage.R
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageException
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import com.google.firebase.storage.ktx.storageMetadata
+//import kotlinx.android.synthetic.main.activity_storage.imageView
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
+
 class HomePage : AppCompatActivity() {
 
     private val PERMISSION_CODE = 1000;
@@ -19,19 +38,28 @@ class HomePage : AppCompatActivity() {
     private val IMAGE_PICK_CODE = 1002;
     var image_uri: Uri? = null
 
+    private lateinit var auth: FirebaseAuth
+    lateinit var storage: FirebaseStorage
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        storage = Firebase.storage
+        //auth = Firebase.auth
+
+        //family-photo-share-41b26
+        //AIzaSyBG0SNhEX7bTfKcekJ6ffbEM5a8dX7uprQ
 
         //Button click
         viewPictures.setOnClickListener {
             //this checks runtime permissions
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
                     PackageManager.PERMISSION_DENIED
                 ) {
 
-                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
                     requestPermissions(permissions, PERMISSION_CODE)
                 } else {
                     //permission has been granted
@@ -46,12 +74,12 @@ class HomePage : AppCompatActivity() {
 
         takePicture.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(android.Manifest.permission.CAMERA)
+                if (checkSelfPermission(Manifest.permission.CAMERA)
                     == PackageManager.PERMISSION_DENIED ||
-                    checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     == PackageManager.PERMISSION_DENIED) {
                     //permission was not enabled
-                    val permission = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     requestPermissions(permission, PERMISSION_CODE)
                 }
                 else {
@@ -62,6 +90,168 @@ class HomePage : AppCompatActivity() {
                 openCamera()
             }
         }
+
+        uploadPictures.setOnClickListener {
+            uploadImage()
+        }
+    }
+
+    private fun includesForCreateReference() {
+        val storage = Firebase.storage
+
+        // ## Create a Reference
+
+        // [START create_storage_reference]
+        // Create a storage reference from our app
+        var storageRef = storage.reference
+        // [END create_storage_reference]
+
+        // [START create_child_reference]
+        // Create a child reference
+        // imagesRef now points to "images"
+        var imagesRef: StorageReference? = storageRef.child("images")
+
+        // Child references can also take paths
+        // spaceRef now points to "images/space.jpg
+        // imagesRef still points to "images"
+        var spaceRef = storageRef.child("images/space.jpg")
+        // [END create_child_reference]
+
+        // ## Navigate with References
+
+        // [START navigate_references]
+        // parent allows us to move our reference to a parent node
+        // imagesRef now points to 'images'
+        imagesRef = spaceRef.parent
+
+        // root allows us to move all the way back to the top of our bucket
+        // rootRef now points to the root
+        val rootRef = spaceRef.root
+        // [END navigate_references]
+
+        // [START chain_navigation]
+        // References can be chained together multiple times
+        // earthRef points to 'images/earth.jpg'
+        val earthRef = spaceRef.parent?.child("earth.jpg")
+
+        // nullRef is null, since the parent of root is null
+        val nullRef = spaceRef.root.parent
+        // [END chain_navigation]
+
+        // ## Reference Properties
+
+        // [START reference_properties]
+        // Reference's path is: "images/space.jpg"
+        // This is analogous to a file path on disk
+        spaceRef.path
+
+        // Reference's name is the last segment of the full path: "space.jpg"
+        // This is analogous to the file name
+        spaceRef.name
+
+        // Reference's bucket is the name of the storage bucket that the files are stored in
+        spaceRef.bucket
+        // [END reference_properties]
+
+        // ## Full Example
+
+        // [START reference_full_example]
+        // Points to the root reference
+        storageRef = storage.reference
+
+        // Points to "images"
+        imagesRef = storageRef.child("images")
+
+        // Points to "images/space.jpg"
+        // Note that you can use variables to create child values
+        val fileName = "space.jpg"
+        spaceRef = imagesRef.child(fileName)
+
+        // File path is "images/space.jpg"
+        val path = spaceRef.path
+
+        // File name is "space.jpg"
+        val name = spaceRef.name
+
+        // Points to "images"
+        imagesRef = spaceRef.parent
+        // [END reference_full_example]
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        signIn("tapsfamilyphotos@gmail.com", "BCisit322T@p5")
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+
+                // [START_EXCLUDE]
+                if (!task.isSuccessful) {
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                }
+                // [END_EXCLUDE]
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun uploadImage() {
+//        var file = "1592346486601.jpg"
+        var filepath = "/storage/emulated/0/Pictures/1592346486601.jpg"
+
+        var file = Uri.fromFile(File("/storage/emulated/0/Pictures/1592346486601.jpg"))
+        val storage = Firebase.storage
+
+        // [START upload_create_reference]
+        // Create a storage reference from our app
+        val storageRef = storage.reference
+
+        // Create a reference to "mountains.jpg"
+        val mountainsRef = storageRef.child("1592346486601.jpg")
+
+        // Create a reference to 'images/mountains.jpg'
+        val mountainImagesRef = storageRef.child(filepath)
+
+        // While the file names are the same, the references point to different files
+        mountainsRef.name == mountainImagesRef.name // true
+        mountainsRef.path == mountainImagesRef.path // false
+        // [END upload_create_reference]
+
+        // [START upload_memory]
+        // Get the data from an ImageView as bytes
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = mountainsRef.putBytes(data)
+
+        // [START upload_file]
+        val riversRef = storageRef.child("images/${file.lastPathSegment}")
+        uploadTask = riversRef.putFile(file)
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener {
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
+        // [END upload_file]
     }
 
     private fun openCamera() {
@@ -78,15 +268,6 @@ class HomePage : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, IMAGE_PICK_CODE)
-    }
-
-    companion object {
-        //image pick code
-        private val IMAGE_PICK_CODE = 1000;
-
-        //Permission Code
-        private val PERMISSION_CODE = 1001;
-
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -113,9 +294,9 @@ class HomePage : AppCompatActivity() {
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             imageView.setImageURI(data?.data)
         }
-        else if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_CAPTURE_CODE) {
-            imageView.setImageURI(data?.data)
-        }
+//        else if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_CAPTURE_CODE) {
+//            imageView.setImageURI(data?.data)
+//        }
     }
 
 }
